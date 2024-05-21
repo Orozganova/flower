8fda3d8fd75434b211d2a0760e59a19a9f3c8000
Orozganova <aorozganova@gmail.com>
2024-05-21 22:32:02 +0600

    added section in app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Home from "./pages/Home";
+import Category from "./pages/Category";
+import { createContext, useEffect, useState } from "react";
 
-function App() {
+
+import { onAuthChange, onCategoriesLoad, onOrdersLoad, onProductsLoad } from "./firebase";
+import Cart from "./pages/Cart";
+import NotFound from "./pages/NotFound";
+import Product from "./pages/Product";
+import ThankYou from "./pages/ThankYou";
+import Contacts from "./pages/Contacts"
+import About from "./pages/About";
+import Delivery from "./pages/Delivery";
+import Orders from "./pages/Orders";
+
+export const AppContext = createContext({
+  categories: [],
+  products: [],
+  oreder: [],
+
+  // корзина
+  cart: {},
+  setCart: () => { },
+
+  user: null, // здесь будет храниться информация про пользователя
+});
+
+
+
+export default function App() {
+  const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [orders, setOrders] = useState([]);
+
+  const [cart, setCart] = useState(() => {
+    return JSON.parse(localStorage.getItem("cart")) || {};
+  });
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
+  useEffect(() => {
+    onCategoriesLoad(setCategories);
+    onProductsLoad(setProducts);
+    onOrdersLoad(setOrders);
+
+    onAuthChange((user) => {
+      if (user) {
+        user.isAdmin = user.email === "ajarka1334@gmail.com";
+      }
+      setUser(user);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppContext.Provider value={{ categories, products, cart, setCart, user, orders }}>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contacts" element={<Contacts />} />
+            <Route path="/delivery" element={<Delivery />} />
+            <Route path="/category/:path" element={<Category />} />
+            <Route path="/product/:path" element={<Product />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/thank-you" element={<ThankYou />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </AppContext.Provider>
     </div>
   );
 }
-
-export default App;
